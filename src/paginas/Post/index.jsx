f9efdfe2867cd8841b1d2cc8e85a68c0d1925698
ledsouza <leandro.souza.159@gmail.com>
2024-07-04 15:44:09 -0300

    implementada as páginas de post

--- src/paginas/Post/index.jsx
+++ src/paginas/Post/index.jsx
@@ -1,6 +1,18 @@
+import ReactMarkdown from "react-markdown";
 import { useParams } from "react-router-dom";
+import PostModelo from "src/componentes/PostModelo";
+import posts from "src/json/posts.json";
+import styles from "./Post.module.css";
 
 export default function Post() {
     const parametros = useParams();
-    return <h2>Conteúdo do Post {parametros.id}</h2>;
+    const post = posts.find((post) => post.id === Number(parametros.id));
+
+    return (
+        <PostModelo fotoCapa={`/assets/posts/${post.id}/capa.png`} titulo={post.titulo}>
+            <div className={styles.postMarkdownContainer}>
+                <ReactMarkdown>{post.texto}</ReactMarkdown>
+            </div>
+        </PostModelo>
+    );
 }
